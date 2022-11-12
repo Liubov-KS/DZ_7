45b4e82b9a30af3e59be9fc0d5ea4da6c38fe9bd
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-11-12 17:17:45 +0300

    add 47 task

--- Program.cs
+++ Program.cs
@@ -5,7 +5,44 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+Console.WriteLine("Задайте количество строк двумерного массива:"); // m
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Задайте количество столбцов двумерного массива:"); // n
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] twoDimArray = new double[m, n];
+
+Random rnd = new Random();
+
+void PrintArray(double[,] matrix)
+{ 
+    for (int i = 0; i < m; i++)
+    { 
+    for (int j = 0; j < n; j++)
+        { 
+        Console.Write($"{matrix[i, j]} ");
+        }
+ Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{ 
+    for (int i = 0; i < m; i++)
+    { 
+    for (int j = 0; j < n; j++)
+        { 
+         matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);  // конверт матриц в дабл тип чисел
+        }
+    }
+}
+
+FillArray(twoDimArray);
+
+Console.WriteLine();
+
+PrintArray(twoDimArray);
 
 
 
