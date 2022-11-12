c2541c2d91095c5a8c3879e60e0100130cdd9a2c
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-11-12 17:32:23 +0300

    add task 50

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
-Console.WriteLine("Задайте количество строк двумерного массива:"); // m
+/* Console.WriteLine("Задайте количество строк двумерного массива:"); // m
 int m = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Задайте количество столбцов двумерного массива:"); // n
@@ -43,7 +43,7 @@ FillArray(twoDimArray);
 Console.WriteLine();
 
 PrintArray(twoDimArray);
-
+*/
 
 
 //Задача 50.
@@ -54,7 +54,36 @@ PrintArray(twoDimArray);
 5 9 2 3
 8 4 2 4                    17 -> такого числа в массиве нет
 */
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,10));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите позицию элемента в двумерном массиве");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ 
+ if (a > m && b > n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
 
 
 
